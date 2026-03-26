df56997b923d57858fb9f5ea56c5c48bffde53b2
337ubaid <337ubaid@gmail.com>
2026-03-26 09:20:47 +0700

    add segmen on am page, add function cari_am

--- pages/tunggakan_am.py
+++ pages/tunggakan_am.py
@@ -11,46 +11,43 @@ from ui.metrics import render_dashboard_metrics
 from datetime import date
 from core.constant import COLUMNS_TUNGGAKAN_AM, COLUMNS_KUADRAN
 from services.kuadran_service import prepare_kuadran_data
+from utils.selector import pilih_segmen, cari_am
+
 
 TODAY = date.today()
 render_sidebar()
+df_database = load_database_nonpots()
+latest_date = df_database["tanggal"].max()
 
 # ====== Konfigurasi Page ======
 st.set_page_config(
     page_title="Dashboard Data Collection Jatim Barat", layout="wide", page_icon="📈"
 )
 st.title("❇️ Tunggakan AM")
+# ==============================
 
-df_database = load_database_nonpots()
-latest_date = df_database["tanggal"].max()
+# FILTERING
+segmen_target = pilih_segmen()
+filtered_df = cari_am(df_database)
 
-
-nama_am = st.text_input("Nama AM", "")
-if nama_am:
-    filtered_df = df_database[
-        df_database["nama_am"].str.contains(nama_am, case=False, na=False)
-    ]
-else:
-    filtered_df = df_database
-
+# METRICS
 render_dashboard_metrics(filtered_df)
 
-tab_summary, tab_kuadran = st.tabs(["Summary", "Kuadran"])
+# KUADRAN & SUMMARY
+tab_kuadran, tab_summary = st.tabs(["Kuadran", "Summary"])
 
-filtered_df = filter_collection_data(filtered_df, "-Semua-", tanggal=latest_date)
+filtered_df = filter_collection_data(filtered_df, segmen_target, tanggal=latest_date)
 
+with tab_kuadran:
+    df_kuadran, total_pelanggan, total_saldo = prepare_kuadran_data(
+        filtered_df, segmen_target, COLUMNS_KUADRAN
+    )
 
+    render_all_kuadran(df_kuadran, total_pelanggan, total_saldo)
 with tab_summary:
     print_sort_dataframe(filtered_df[COLUMNS_TUNGGAKAN_AM])
 
-with tab_kuadran:
-    filtered_df, total_pelanggan, total_saldo = prepare_kuadran_data(
-        filtered_df, "-Semua-", COLUMNS_KUADRAN
-    )
-
-    render_all_kuadran(filtered_df, total_pelanggan, total_saldo)
-    pass
-
+# DETAIL KUADRAN
 st.divider()
 st.subheader("Detail Kuadran")
 
--- utils/selector.py
+++ utils/selector.py
@@ -7,3 +7,12 @@ def pilih_segmen():
     # Pilihan segmen
     segmen_target = st.selectbox("Pilih Segmen", SEGMEN, key="segmen_input")
     return segmen_target
+
+
+def cari_am(df):
+    nama_am = st.text_input("Nama AM", "")
+    if nama_am:
+        filtered_df = df[df["nama_am"].str.contains(nama_am, case=False, na=False)]
+    else:
+        filtered_df = df
+    return filtered_df
